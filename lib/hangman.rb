16712da62d95203397c99e0d88b63b05bebffb79
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-06-25 15:15:26 -0400

    Add #get_match function
    When implemented this commit will add the #get_match function. The get match function takes the user's guess in the form of a letter or word and determines if there is a match with the secret word.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -5,13 +5,19 @@ class Hangman
   
   # Initialize the game object 
   def initialize
-    @word = choose_word
+    # Variable stores word user must guess
+    @word = "cat"
 
-    puts @word.length
+    # Counter keeps track of how many guesses user has made (User gets 6 max guesses)
+    @counter = 0 
 
+    # Array to hold previous user guesses
+    @past_guesses = []
+
+    # Array will hold "blank" spaces and be updated with users correct guesses 
     @displayed_word = Array.new(@word.length, "_ ")
 
-    get_letter_or_guess
+    check_for_match("cat")
     
    #intro_text
   end 
@@ -48,16 +54,46 @@ class Hangman
     word 
   end
 
-  def get_letter_or_guess
-    puts ("Pick a letter: ")
-    letter = gets.chomp.downcase
+  # Get user guess in form of letter or word 
+  def get_guess
+    puts ("Pick a guess or try to guess the word: ")
+    guess = gets.chomp.downcase
 
-    until /^[a-z]{1}$/.match(letter).to_s == letter || /^[a-z]{#{@word.length}}$/.match(letter).to_s == letter
-      puts "Please enter a valid letter or a valid guess (The secret word is #{@word.length} letters long)"
+    # Keep prompting for input unless user provides a letter or  provides a word that is same length as secret word 
+    until /^[a-z]{1}$/.match(guess).to_s == guess || /^[a-z]{#{@word.length}}$/.match(guess).to_s == guess
+      puts "Please enter a valid guess or a valid guess (The secret word is #{@word.length} guesss long)"
 
-      letter = gets.chomp.downcase
+      guess = gets.chomp.downcase
     end 
   end 
+
+  def check_for_match(guess)
+    # If guess is a word and does not match secret word, increase counter 
+    if guess.length > 1 && guess != @word
+      @counter += 1
+      past_guesses.push(guess)
+      return 
+    elsif guess.length > 1 && guess == word
+      @displayed_word = guess
+      return 
+    end 
+
+    # If guess is a letter, check if it matches any letters in the secret word
+    right_letter = (0..word.length).select do |index|
+      # If letter is a match return index(s) that are a match 
+      @word[index] == letter
+    end  
+
+    if right_letter.length == 0
+      @counter += 1
+      past_guesses.push(guess)
+    end 
+
+    right_letter.each do |index|
+      @displayed_word[index] = "#{gues} "
+    end 
+  end 
+
 
 end 
 
